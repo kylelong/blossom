d33af7bf69a53ae74f6aba6d966240e3b1ee06cc
kyle long <kylel95@vt.edu>
2022-11-28 00:15:22 -0800

    add emoji

--- src/dashboard/surveyPreview/Emojis.js
+++ src/dashboard/surveyPreview/Emojis.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Emojis = () => {
   const emojis = {
-    angry: "&#128545",
+    angry: "0x1F621",
+    sad: "0x1F614",
+    neutral: "0x1F611",
+    happy: "0x1F60A",
+    love: "0x1F60D",
+  };
+  const [emoji, setEmoji] = useState("");
+  const handleEmojis = (key) => {
+    if (Object.keys(emojis).includes(key)) {
+      setEmoji(key);
+    }
   };
   return (
     <div className="emojiContainer">
-      <div className="emoji">&#128545;</div>
-      <div className="emoji">&#128532;</div>
-      <div className="emoji">&#128529;</div>
-      <div className="emoji">&#128522;</div>
-      <div className="emoji">&#128525;</div>
+      {Object.entries(emojis).map(([key, value]) => {
+        if (key === emoji) {
+          return (
+            <div
+              className="emojiSelected"
+              key={key}
+              onClick={() => handleEmojis(key)}
+            >
+              {String.fromCodePoint(value)}
+            </div>
+          );
+        } else {
+          return (
+            <div className="emoji" key={key} onClick={() => handleEmojis(key)}>
+              {String.fromCodePoint(value)}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
