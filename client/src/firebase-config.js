42b6250a4013f99536b98d9ddfd963b4b0e87531
kyle long <kylel95@vt.edu>
2023-04-10 15:03:40 -0400

    add env variables for firebase to .env file

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -5,13 +5,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWN8dGYlAeYID1lv7Sc-CENj9m-RL4PBM",
-  authDomain: "blossom-8a8cb.firebaseapp.com",
-  projectId: "blossom-8a8cb",
-  storageBucket: "blossom-8a8cb.appspot.com",
-  messagingSenderId: "313045377208",
-  appId: "1:313045377208:web:af52580468dca0e43c944d",
-  measurementId: "G-T0W2YLF0YZ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
