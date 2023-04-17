de078fe7e62a637b34f58bf4f34931c70e8403cb
kyle long <kylel95@vt.edu>
2023-04-17 02:58:31 -0400

    fix typo

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    process.env.REACT_APP_NODE_ENV === "production"
+    process.env.NODE_ENV === "production"
       ? process.env.LIVE_URL
       : process.env.LOCAL_URL
   );
